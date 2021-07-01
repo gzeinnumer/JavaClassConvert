2e951da6caebe42dfd988c364927b1fd36592fd7
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-07-01 09:07:49 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/javaclassconvert/MainActivity.java
@@ -0,0 +1,67 @@
+package com.gzeinnumer.javaclassconvert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<ModelTarget> data = convetModel1ToModel2();
+
+        for (int i = 0; i < data.size(); i++) {
+            Log.d(getClass().getSimpleName(), "onCreate: "+data.toString());
+        }
+    }
+
+    private ArrayList<ModelTarget> convetModel1ToModel2() {
+        ArrayList<ModelSource> listSource = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            listSource.add(new ModelSource(i, i+"_ModelSource"));
+        }
+
+        ArrayList<ModelTarget> listTarget = new ArrayList<>();
+        for (int i = 0; i < listSource.size(); i++) {
+            listTarget.add(new ModelTarget(listSource.get(i)));
+        }
+
+        listSource.clear();
+        return listTarget;
+    }
+}
+
+class ModelSource {
+
+    public int id;
+    public String name;
+
+    public ModelSource() {}
+
+    public ModelSource(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class ModelTarget extends ModelSource {
+
+    public ModelTarget(ModelSource data) {
+        this.id = data.id;
+        this.name = data.name;
+    }
+
+    @Override
+    public String toString() {
+        return "ModelTarget{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
